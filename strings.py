b1ba385c1db7528ff5abd006d08f3672c43244a2
G Tharun Kumar reddy <93675638+Tharunreddygtr@users.noreply.github.com>
2024-07-17 10:46:08 +0530

    Update strings.py

--- strings.py
+++ strings.py
@@ -21,3 +21,29 @@ def lengthOfLongestSubstring(s):
 
 s = "abcabcbb"
 print(lengthOfLongestSubstring(s))
+
+
+def characterReplacement(s, k):
+    c_frequency = {}
+    max_str = 0
+    left = 0
+    right = 0
+    while left < len(s) and right < len(s):
+        if not s[right] in c_frequency:
+            c_frequency[s[right]] = 0
+        c_frequency[s[right]] += 1
+        cells_count = right - left + 1
+        if cells_count - max(c_frequency.values()) <= k:
+            max_str = max(max_str, cells_count)
+            right += 1
+        else:
+            c_frequency[s[left]] -= 1
+            left += 1
+            right += 1
+
+    return max_str
+
+
+s = "AABABBA"
+k = 1
+print(characterReplacement(s, k))

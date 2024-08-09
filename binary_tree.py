5cff2890a2685e02d16ef429875f9c4916c42846
G Tharun Kumar reddy <93675638+Tharunreddygtr@users.noreply.github.com>
2024-08-09 11:50:30 +0530

    added diagonal view of a binary tree

--- binary_tree.py
+++ binary_tree.py
@@ -469,3 +469,57 @@ items = {'apple': 12, 'banana': 7, 'cherry': 10, 'dragonfruit': 35}
 selected = ['apple', 'apple', 'cherry', 'eggplant', 'cherry']
 cost = get_total_cost(items, selected)
 print(cost)
+
+
+
+
+def diagonalPrintUtil(root, d, diagonalPrintMap):
+    # Base Case
+    if root is None:
+        return
+
+    # Store all nodes of same line
+    # together as a vector
+    try:
+        diagonalPrintMap[d].append(root.data)
+    except KeyError:
+        diagonalPrintMap[d] = [root.data]
+
+    # Increase the vertical distance
+    # if left child
+    diagonalPrintUtil(root.left,
+                      d + 1, diagonalPrintMap)
+
+    # Vertical distance remains
+    # same for right child
+    diagonalPrintUtil(root.right,
+                      d, diagonalPrintMap)
+
+
+# Print diagonal traversal of given binary tree
+def diagonalPrint(root):
+    # Create a dict to store diagonal elements
+    diagonalPrintMap = dict()
+
+    # Find the diagonal traversal
+    diagonalPrintUtil(root, 0, diagonalPrintMap)
+
+    print("Diagonal Traversal of binary tree : ")
+    for i in diagonalPrintMap:
+        for j in diagonalPrintMap[i]:
+            print(j, end=" ")
+        print()
+
+
+# Driver Program
+root = Node(8)
+root.left = Node(3)
+root.right = Node(10)
+root.left.left = Node(1)
+root.left.right = Node(6)
+root.right.right = Node(14)
+root.right.right.left = Node(13)
+root.left.right.left = Node(4)
+root.left.right.right = Node(7)
+
+diagonalPrint(root)

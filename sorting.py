40aabb1b595919d2c8071c8410f05622d858fd8b
G Tharun Kumar reddy <93675638+Tharunreddygtr@users.noreply.github.com>
2024-08-14 09:29:21 +0530

    Update sorting.py

--- sorting.py
+++ sorting.py
@@ -7,7 +7,7 @@ def quick_sort_fun(quick_sort):
     if not quick_sort:
         return []
     mid = quick_sort[0]
-    left_list = [i for i in quick_sort if i < mid]
+    left_list = [i for i in quick_sort if i <= mid]
     right_list = [i for i in quick_sort if i > mid]
 
     return quick_sort_fun(left_list) + [mid] + quick_sort_fun(right_list)

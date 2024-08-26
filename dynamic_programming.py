f3b12aa0785cfde177966a3225f3d66a8875b123
G Tharun Kumar reddy <93675638+Tharunreddygtr@users.noreply.github.com>
2024-08-26 12:44:52 +0530

    Update dynamic_programming.py

--- dynamic_programming.py
+++ dynamic_programming.py
@@ -81,3 +81,18 @@ n = len(values)
 max_value = knapsack_recursive(values, weights, W, n)
 print("Maximum value in knapsack:", max_value)
 
+# house robber 198 leetcode
+def rob(nums):
+    length = len(nums)
+    if length == 1:
+        return sum(nums)
+    elif length == 2:
+        return max(nums[0], nums[1])
+    dp = [0]*length
+    dp[0] = nums[0]
+    dp[1] = max(nums[1], nums[0])
+    for i in range(2, length):
+        dp[i] = max(dp[i-1], dp[i-2]+nums[i])
+    return dp[length-1]
+nums = [1,2,3,1]
+print(rob(nums))

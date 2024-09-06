5bad9b5fabb955b940638394446040f395f8a56f
G Tharun Kumar reddy <93675638+Tharunreddygtr@users.noreply.github.com>
2024-09-06 07:42:22 +0530

    Update cache.py

--- cache.py
+++ cache.py
@@ -57,3 +57,67 @@ c.put("demo", 3)
 c.get("world")
 c.put("demo1", 4)
 c.put("demo2", 5)
+
+
+# LRU cache O(1) time complexity
+class Node:
+    def __init__(self, key, value):
+        self.key =  key
+        self.value = value
+        self.prev = None
+        self.next = None
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = {}
+        self.head = Node(0,0)
+        self.tail = Node(0,0)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    def remove(self, node):
+        prev = node.prev
+        next = node.next
+        prev.next = next
+        next.prev = prev
+
+    def add(self, node):
+        prev = self.tail.prev
+        prev.next = node
+        node.prev = prev
+        node.next = self.tail
+        self.tail.prev = node
+
+        
+
+    def get(self, key: int) -> int:
+        value = self.cache.get(key)
+        if value:
+            node = self.cache[key]
+            self.remove(node)
+            self.add(node)
+            return node.value
+        else:
+            return -1
+        
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.remove(self.cache[key])
+        node = Node(key, value)
+        self.add(node)
+        self.cache[key] = node
+        if len(self.cache.keys()) > self.capacity:
+            lru = self.head.next
+            self.remove(lru)
+            del self.cache[lru.key]
+
+        
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)

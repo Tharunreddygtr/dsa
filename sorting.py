f410eba72b25fc853d67cd39c98c8cb42cc40645
G Tharun Kumar reddy <93675638+Tharunreddygtr@users.noreply.github.com>
2025-01-15 14:41:13 +0530

    Update sorting.py

--- sorting.py
+++ sorting.py
@@ -57,3 +57,28 @@ def merge_sort(arr):
             arr[k] = right_half[j]
             j += 1
             k += 1
+
+
+
+
+def sortColors(nums):
+    high = len(nums) - 1
+    low = 0
+    mid = 0
+
+    while mid <= high:
+        if nums[mid] == 0:
+            nums[mid], nums[low] = nums[low], nums[mid]
+            low += 1
+            mid += 1
+        elif nums[mid] == 1:
+            mid += 1
+        elif nums[mid] == 2:
+            nums[mid], nums[high] = nums[high], nums[mid]
+            high -= 1
+
+
+print(sortColors([2, 0, 1, 2, 0, 1]))
+
+
+

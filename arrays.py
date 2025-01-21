d9c6f69e9215c5d53036f8462a34b8be2bf40cbc
G Tharun Kumar reddy <93675638+Tharunreddygtr@users.noreply.github.com>
2025-01-21 13:00:05 +0530

    Update arrays.py

--- arrays.py
+++ arrays.py
@@ -71,5 +71,38 @@ def longestPalindrome(s):
             left-=1
             right+=1
     return string
-        
+
+# find median in sorted array in O(log(n+m)) time complexity
+def findMedianSortedArrays(nums1, nums2):
+    n = len(nums1)
+    m = len(nums2)
+    i = 0
+    j = 0
+    m1 = 0
+    m2 = 0
+
+    # Find median.
+    for count in range(0, (n + m) // 2 + 1):
+        m2 = m1
+        if i < n and j < m:
+            if nums1[i] > nums2[j]:
+                m1 = nums2[j]
+                j += 1
+            else:
+                m1 = nums1[i]
+                i += 1
+        elif i < n:
+            m1 = nums1[i]
+            i += 1
+        else:
+            m1 = nums2[j]
+            j += 1
+
+    # Check if the sum of n and m is odd.
+    if (n + m) % 2 == 1:
+        return float(m1)
+    else:
+        ans = float(m1) + float(m2)
+        return ans / 2.0
+print(findMedianSortedArrays([1,2], [3]))
 

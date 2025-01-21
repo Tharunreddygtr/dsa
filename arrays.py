155d1b5f1971d73c6255828db86903d6126470a6
G Tharun Kumar reddy <93675638+Tharunreddygtr@users.noreply.github.com>
2025-01-21 15:38:47 +0530

    Update arrays.py

--- arrays.py
+++ arrays.py
@@ -106,3 +106,20 @@ def findMedianSortedArrays(nums1, nums2):
         return ans / 2.0
 print(findMedianSortedArrays([1,2], [3]))
 
+
+
+def increasingTriplet(nums):
+    first = float("inf")
+    second = float("inf")
+    for num in nums:
+        if num <= first:
+            first = num
+        elif num <= second:
+            second = num
+        else:
+            return True
+    return False
+nums = [1,2,3,4,5,6]
+print(increasingTriplet(nums))
+# output True 
+

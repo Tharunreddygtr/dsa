1f0dae2fb9664c64aa29bf84784b55ffc1242cec
G Tharun Kumar reddy <93675638+Tharunreddygtr@users.noreply.github.com>
2025-01-31 11:56:25 +0530

    Update arrays.py

--- arrays.py
+++ arrays.py
@@ -159,11 +159,23 @@ print(dailyTemperatures(temperatures))
 
 
 
+# Time complexity O(nlogn)
+def insert(intervals, newInterval):
+    intervals.append(newInterval)
+    intervals.sort()
 
+    res = [intervals[0]]
 
+    for i in range(1, len(intervals)):
+        if res[-1][1] >= intervals[i][0]:
+            res[-1][1] = max(res[-1][1], intervals[i][1])
+        else:
+            res.append(intervals[i])
 
-
-
+    return res
+ intervals = [[1,3],[6,9]]
+newInterval = [2,5]
+print(insert(intervals, newInterval))
 
 
 

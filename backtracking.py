20de2a29ecbd2ffe1b8a0647477332fd79867c4b
G Tharun Kumar reddy <93675638+Tharunreddygtr@users.noreply.github.com>
2025-02-04 15:14:50 +0530

    Update backtracking.py

--- backtracking.py
+++ backtracking.py
@@ -40,3 +40,30 @@ def permute(nums):
 
 nums = [1, 2, 3]
 print(permute(nums))
+
+
+# search word in the 2D list of letters
+def exist(board, word):
+    def backtrack(i, j, k):
+        if k == len(word):
+            return True
+        if i < 0 or i >= len(board) or j < 0 or j >= len(board[0]) or board[i][j] != word[k]:
+            return False
+        
+        temp = board[i][j]
+        board[i][j] = ''
+        
+        if backtrack(i+1, j, k+1) or backtrack(i-1, j, k+1) or backtrack(i, j+1, k+1) or backtrack(i, j-1, k+1):
+            return True
+        
+        board[i][j] = temp
+        return False
+    
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if backtrack(i, j, 0):
+                return True
+    return False
+board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]]
+word = "ABCCED"
+# Output: true

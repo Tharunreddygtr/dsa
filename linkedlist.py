1912049347b6889478f5cf4146183defbae69fda
G Tharun Kumar reddy <93675638+Tharunreddygtr@users.noreply.github.com>
2025-02-04 14:00:48 +0530

    Update linkedlist.py

--- linkedlist.py
+++ linkedlist.py
@@ -309,7 +309,29 @@ def addTwoNumbers(l1, l2):
 
 
         
+# simple solution compared to the above solution
+def addTwoNumbers(l1, l2):
+    dummy = ListNode()
+    res = dummy
+
+    total = carry = 0
+
+    while l1 or l2 or carry:
+        total = carry
+
+        if l1:
+            total += l1.val
+            l1 = l1.next
+        if l2:
+            total += l2.val
+            l2 = l2.next
         
+        num = total % 10
+        carry = total // 10
+        dummy.next = ListNode(num)
+        dummy = dummy.next
+    
+    return res.next
 
 
 
